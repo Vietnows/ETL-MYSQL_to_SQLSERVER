a04d171a781311b8e83745f9486dfd2a30ebbdd3
Marcelo Oliveira <marcello.henrique1994@gmail.com>
2020-01-12 23:13:37 -0300

    Update and rename ConnectionMagentoIcommBI.py to ConnectionMagentoBI.py

--- ConnectionMagentoBI.py
+++ ConnectionMagentoBI.py
@@ -22,8 +22,8 @@ userOQV = '##########'
 passwOQV =  '##########'
 databaseOQV = '##########'
 
-#Configs para ICOMMBI SQL SERVER
-connectionBI = pyodbc.connect('DRIVER='+ driverBI+';SERVER='+serverBI+';DATABASE='+databaseBI+';UID='+userKallas+';PWD='+passwKallas)
+#Keys to Connecto on SQL SERVER
+connectionBI = pyodbc.connect('DRIVER='+ driverBI+';SERVER='+serverBI+';DATABASE='+databaseBI+';UID='+userBI+';PWD='+passwBI)
 cursor = connectionBI.cursor()
 
 #Configs para Magento mysql
@@ -33,13 +33,13 @@ connectionOQV = pyodbc.connect('DRIVER='+ driverMySQL+';SERVER='+hostOQV+';DATAB
 queryS2G = "select 'S2G' as canal,produtos.sku as produto,produtos_pai.sku as produto_pai,nome_produto.value descricao,nome_colecao.value as colecao,nome_marca.value as marca,nome_genero.value genero,aviseme.* from product_alert_stock aviseme left join catalog_product_entity produtos on aviseme.product_id = produtos.entity_id left join catalog_product_entity produtos_pai on aviseme.parent_id = produtos_pai.entity_id left join catalog_product_entity_int colecao on produtos.entity_id = colecao.entity_id left join eav_attribute_option_value nome_colecao on colecao.value = nome_colecao.option_id left join catalog_product_entity_int marca on produtos.entity_id = marca.entity_id left join eav_attribute_option_value nome_marca on marca.value = nome_marca.option_id left join catalog_product_entity_varchar nome_produto on produtos.entity_id = nome_produto.entity_id left join catalog_product_entity_int genero on produtos.entity_id =  genero.entity_id left join eav_attribute_option_value nome_genero on genero.value = nome_genero.option_id where colecao.attribute_id = 228 and marca.attribute_id = 239 and nome_produto.attribute_id = 65 and genero.attribute_id = 182 and date(aviseme.add_date) = subdate(curdate(), 1)" 
 queryOQV = "select 'OQV' as canal,produtos.sku as produto,produtos_pai.sku as produto_pai,nome_produto.value descricao,nome_colecao.value as colecao,nome_marca.value as marca,'FEMININO' as genero,aviseme.* from product_alert_stock aviseme left join catalog_product_entity produtos on aviseme.product_id = produtos.entity_id left join catalog_product_entity produtos_pai on aviseme.parent_id = produtos_pai.entity_id left join catalog_product_entity_int colecao on produtos.entity_id = colecao.entity_id left join eav_attribute_option_value nome_colecao on colecao.value = nome_colecao.option_id left join catalog_product_entity_int marca on produtos.entity_id = marca.entity_id left join eav_attribute_option_value nome_marca on marca.value = nome_marca.option_id left join catalog_product_entity_varchar nome_produto on produtos.entity_id = nome_produto.entity_id where colecao.attribute_id = 228 and marca.attribute_id = 239 and nome_produto.attribute_id = 65 and date(aviseme.add_date) = subdate(curdate(), 1)"
 
-#Select dados Magento
+#Select from Magento
 print('Querying Magento MySQL')
 resultOQV = pd.read_sql_query(queryOQV,connectionOQV)
 resultS2G = pd.read_sql_query(queryS2G,connectionS2G)
 
 
-#tratando os dados no Pandas Python
+#Python Pandas treatment
 #df_all['add_date'] = pd.to_datetime(df_all['add_date'])
 dfs = [resultOQV, resultS2G]
 df_all = pd.concat(dfs,ignore_index=True)
@@ -47,7 +47,7 @@ df_all.drop('send_date', axis=1,inplace=True)
 df_all = df_all.fillna(0)
 df_all['store_id'] = df_all['store_id'].apply(int) 
 
-#Inserindo Dados no Icommbi
+#Insert data from Magento in SQL SERVER
 print('insert data ICOMMBI ')
 for index, row in df_all.iterrows():
     cursor.execute("INSERT INTO AVISE_ME(canal,produto,produto_pai,descricao,colecao,marca,genero,alert_stock,customer_id,product_id,website_id,send_count,status,parent_id,email,store_id,add_date) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
